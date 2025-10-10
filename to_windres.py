6b095a51e5f6135f32a0d63ca7da98602b72f2ab
unknown <kindermolina98@gmail.com>
2025-10-10 14:58:34 -0300

    problem inserting the new BEGIN-END block;

--- to_windres.py
+++ to_windres.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 def extract():
     cwd = os.getcwd()
@@ -22,14 +23,70 @@ def extract():
                 controls.append(line)
         return True, controls
 
-def controls(ctrl):
-    print("thinking on")
+def modify(ctrls):
+    ctr_class = ["BUTTON", "COMBOBOX", "STATIC"] 
+    BS  = ["BS_DEFPUSHBUTTON", "BS_PUSHBUTTON"]
+    CBS = ["CBS_DROPDOWN"]
+    SS  = ["SS_LEFT"]
+    
+    classic = ""
+    j = 0
+    for ctrl in ctrls:
+        ctrl = ctrl.replace("\x00", "")
+        listed = ctrl.split(",")
+        if (j % 2 == 0) and (j!=0) :
+            caption = listed[0].split('"')[1]
+            ID      = listed[1]
+            style   = ""
+            classy  = ""
 
+            third = listed[3].replace("\x00", "")
+            words = third.split(" ")
+            for bs in BS:
+                if bs in words:
+                    classy = bs[3:]
+                    bs = "{} |".format(bs)
+                    style = third.replace(bs, "")
+            for ss in SS:
+                if ss in words:
+                    classy = ss[3:]
+                    ss = "{} |".format(ss)
+                    style = third.replace(ss, "")
+            for cbs in CBS:
+                if cbs in words:
+                    classy = cbs[4:]
+                    cbs = "{} |".format(cbs)
+                    style = third.replace(cbs, "")
+            dims = listed[-4:]
+            newline = "{} \"{}\", {},".format(classy, caption, ID)
+            for di in dims:
+                newline = "{} {},".format(newline, di)
+            newline = "{} {}".format(newline, style)
+            classic = "{}\n{}".format(classic, newline)
+        j=j+1
+    return classic
+
+
+def convert(ctrl):
+    res = "geoelectrical\\resource.rc"
+    path = "{}\{}".format(os.getcwd(), res)
+    with open(path, "r", encoding="latin1") as f:
+        cnt = f.read()
+        prev= cnt.split("BEGIN")
+        print(prev)
+        last= cnt.split("END")
+        classic = modify(ctrl)
+        modified= "{}\n{}\n{}".format(prev, classic, last)
+        return modified
 
 if __name__ == "__main__":
     res, controls = extract()
     if res == True:
-        convert(controls)
+        converted = convert(controls)
+        with open("prove.rc", "w") as f:
+            f.write(converted)
+
+
 
 
 
